ae3adcc284a8c148770d2630c66139259ad158a1
Nauman Khan <naumankhan642@gmail.com>
2022-06-30 21:10:57 +0500

    added before_validation callback & validate email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ApplicationRecord
+  validates :email, presence: true
   has_one :rolify
   has_one :role, through: :rolify
   has_one :store
   has_many :orders
   has_many :products
   has_many :comments, as: :commentable
+  before_validation :check_email
+
+  def check_email
+    if self.email.nil?
+      self.email = ENV['email']
+    end
+  end
 end
