f9b0145690e116ebe40df077d052a11280af6be3
NaumanXcorbit <nauman.khan@xcorebit.com>
2022-07-01 16:57:49 +0500

    added after_destroy callback to delete lineItems when there product is deleted

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,11 @@
 class Product < ApplicationRecord
   belongs_to :user
-  has_many :line_items
+  has_many :line_items, dependent: :destroy
   has_many :orders, through: :line_items
   has_and_belongs_to_many :categories
+  after_destroy :delete_line_items
+
+  def delete_line_items
+    self.line_items.where(product_id: self.id).delete_all
+  end
 end
